3894329883296d15cdeff5f5ee2ee0d89f8b3470
Mark Moffat <hi@markmoffat.com>
2019-11-22 09:26:10 +1030

    Removed dependency

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 #!/usr/bin/env node
 
-const commander = require('commander');
-const pkg = require('./package.json');
-const program = new commander.Command();
-
-// Set version
-program.version(pkg.version);
-
-// Parse args
-program.parse(process.argv);
+const args = process.argv.slice(2);
 
 // Execute command with args
-var exec = require('child_process').exec;
-exec(`dir ${program.args}`, { cwd: process.cwd() }, (error, stdout, stderr) => {
+const exec = require('child_process').exec;
+exec(`dir ${args}`, { cwd: process.cwd() }, (error, stdout, stderr) => {
   if(stderr){
     console.log(stderr);
     return;
